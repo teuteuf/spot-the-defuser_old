ef7be22fc71413fb772dc60aa2d6d34fccc6fd1f
Antoine TEUF <antoine.teuf@gmail.com>
2017-05-18 18:53:10 +0200

    Add missing StdNetworkDiscovery...

---
+++ Assets/Scripts/StdNetworkDiscovery.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class StdNetworkDiscovery : NetworkDiscovery
+{
+	public override void OnReceivedBroadcast(string fromAddress, string data)
+	{
+		NetworkManager.singleton.networkAddress = fromAddress;
+		NetworkManager.singleton.StartClient();
+		StopBroadcast();
+	}
+}
